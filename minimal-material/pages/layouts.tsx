ff19733dace039612f2c15a5c06444e3096db8ba
askirmas <kirmas@phystech.edu>
2020-07-10 21:45:14 +0300

    Add grid-based layouts

---
+++ minimal-material/pages/layouts.tsx
@@ -0,0 +1,21 @@
+export default function Layouts() {
+  return <article className="suite_vertical">
+    <section>
+      <div>
+
+      </div>
+    </section>
+    <section>
+      <div className="box-check">
+        <div className="mm_2_tab_item content_favorites"/>
+        <div className="mm_2_tab_item_vert content_favorites"/>
+        <div className="mm_2_icon_button content_favorites"/>
+        <div className="mm_2_fab_icon content_favorites"/>
+        <div className="mm_2_fab_min content_favorites"/>
+        <div className="mm_2_fab content_favorites"/>
+        <div className="mm_2_fab_rev content_favorites"/>
+        <div className="mm_2_button content_favorites"/>
+      </div>
+    </section>
+  </article>
+}
