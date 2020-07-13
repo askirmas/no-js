8dd457a105af711eeb2304a03f93783e197223af
askirmas <kirmas@phystech.edu>
2020-07-13 14:27:39 +0300

    Change palette from sandbox html to tsx

---
+++ minimal-material/pages/palette.tsx
@@ -0,0 +1,21 @@
+import "./palette.scss"
+
+export default function Palette() {
+  return <main>
+    <span></span>
+    <div className="white ov_in0"></div>
+    <div className="colored ov_in0"></div>
+    <div className="white ov_in1"></div>
+    <div className="colored ov_in1"></div>
+    <div className="white ov_in3"></div>
+    <div className="colored ov_in3"></div>
+    <div className="white ov_in2 act"></div>
+    <div className="colored ov_in2"></div>
+    <div className="white ov_in3 act"></div>
+    <div className="colored ov_in3"></div>
+    <div className="white ov_in3"></div>
+    <div className="colored ov_in3 act"></div>
+    <div className="white ov_in2"></div>
+    <div className="colored ov_in2"></div>
+  </main>
+}
