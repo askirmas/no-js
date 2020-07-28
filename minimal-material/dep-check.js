6e3019d96731a2fba43c2879b82fd726367eb775
askirmas <kirmas@phystech.edu>
2020-07-28 15:14:16 +0300

    Delete redundant stuff

--- minimal-material/dep-check.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-require('@cypress/browserify-preprocessor')
--- minimal-material/env.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-const {writeFileSync} = require('fs')
-
-writeFileSync("npm-env.json", JSON.stringify(process.env, null, 2))
