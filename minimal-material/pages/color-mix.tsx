5cd0e86aa01792c8f9aa1a99569d74aa4b8fd503
askirmas <kirmas@phystech.edu>
2020-07-29 21:20:04 +0300

    Add color mix research

---
+++ minimal-material/pages/color-mix.tsx
@@ -0,0 +1,35 @@
+import {Fragment, CSSProperties} from 'react'
+import "./color-mix.scss"
+
+const lightStarts = [25, 50, 75]
+, rgbs = [
+  [53, 0, 128],
+  [106, 0, 255],
+  [181, 128, 255],
+]
+, mixes = [25, 50, 75]
+, lightEnds = [0, 25, 50, 75, 100]
+, schemas = ["rgb", "hsl"]
+
+export default function ColorMixing() {
+  return <article className="color-mix">{
+    schemas.map(schema => <section className={schema}>{
+      lightStarts.map((l0, i) => <section {...{key: l0, className: "lightStart", style: {
+        "--l0": l0,
+        "--red": rgbs[i][0],
+        "--green": rgbs[i][1],
+        "--blue": rgbs[i][2],
+      } as CSSProperties}}>{
+        mixes.map(mix => <section {...{key: mix, className: "mix", style: {"--mix": mix} as CSSProperties}}>{
+          lightEnds.map(l1 => <section {...{key: l1, style: {"--l1": l1} as CSSProperties}}>
+            <div className="overlayed">{`
+              ${l0} ${mix} ${l1}
+            `}</div>
+            <div className="calced"/>
+          </section>)
+        }</section>)
+      }</section>)
+    }</section>)
+  }
+  </article>
+}
