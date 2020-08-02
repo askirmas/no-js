437e3d51ca7e65887330127faa38ebba1cadf500
askirmas <kirmas@phystech.edu>
2020-08-02 14:36:04 +0300

    Add opacity-based colorizer

--- minimal-material/pages/palette.tsx
+++ minimal-material/pages/palette.tsx
@@ -3,19 +3,35 @@ import "./palette.scss"
 export default function Palette() {
   return <article className="palette">
     <span></span>
-    <div className="surface enabled"></div>
-    <div className="colored enabled"></div>
-    <div className="surface hover"></div>
-    <div className="colored hover"></div>
-    <div className="surface focus"></div>
-    <div className="colored focus"></div>
-    <div className="surface selected checked"></div>
-    <div className="colored selected"></div>
-    <div className="surface activated checked"></div>
-    <div className="colored activated"></div>
-    <div className="surface pressed"></div>
-    <div className="colored pressed"></div>
-    <div className="surface dragged"></div>
-    <div className="colored dragged"></div>
+    <div className="fill surface enabled"></div>
+    <div className="fill colored enabled"></div>
+    <div className="fill surface hover"></div>
+    <div className="fill colored hover"></div>
+    <div className="fill surface focus"></div>
+    <div className="fill colored focus"></div>
+    <div className="fill surface selected checked"></div>
+    <div className="fill colored selected"></div>
+    <div className="fill surface activated checked"></div>
+    <div className="fill colored activated"></div>
+    <div className="fill surface pressed"></div>
+    <div className="fill colored pressed"></div>
+    <div className="fill surface dragged"></div>
+    <div className="fill colored dragged"></div>
+
+    <div className="alpha surface enabled"></div>
+    <div className="alpha colored enabled"></div>
+    <div className="alpha surface hover"></div>
+    <div className="alpha colored hover"></div>
+    <div className="alpha surface focus"></div>
+    <div className="alpha colored focus"></div>
+    <div className="alpha surface selected checked"></div>
+    <div className="alpha colored selected"></div>
+    <div className="alpha surface activated checked"></div>
+    <div className="alpha colored activated"></div>
+    <div className="alpha surface pressed"></div>
+    <div className="alpha colored pressed"></div>
+    <div className="alpha surface dragged"></div>
+    <div className="alpha colored dragged"></div>
+    <mark><h1>TBD: Selected@surface has 3 different colors</h1></mark>
   </article>
 }
